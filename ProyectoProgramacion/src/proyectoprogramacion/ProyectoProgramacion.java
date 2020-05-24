d83cfa90c1a38fb152c8ccebc1390e458cb40283
TeaTime <mushashourrea@gmail.com>
2020-05-23 20:57:02 -0400

    push

--- ProyectoProgramacion/src/proyectoprogramacion/ProyectoProgramacion.java
+++ ProyectoProgramacion/src/proyectoprogramacion/ProyectoProgramacion.java
@@ -5,10 +5,14 @@
  */
 package proyectoprogramacion;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -20,9 +24,20 @@ public class ProyectoProgramacion extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLCargarPDF.fxml"));
-        
+        /*
+        stage.setTitle("JavaFX App");
+
+        FileChooser fileChooser = new FileChooser();
+
+        Button button = new Button("Select File");
+        button.setOnAction(e -> {
+            File selectedFile = fileChooser.showOpenDialog(stage);
+        });
+
+        VBox vBox = new VBox(button);
+        Scene scene = new Scene(vBox, 960, 600);*/
         Scene scene = new Scene(root);
-        
+        stage.show();
         stage.setScene(scene);
         stage.show();
     }
