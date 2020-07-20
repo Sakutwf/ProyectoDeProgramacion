7d3852509a465c5907b6592a466fad2a78bf508d
Sakutwf <sakutwf@gmail.com>
2020-07-19 20:57:27 -0400

    Modificación en nombre de atributos clase AdapterRectangulo, para mayor claridad en el txt generado

--- ProyectoProgramacion/src/proyectoprogramacion/AdapterRectangulo.java
+++ ProyectoProgramacion/src/proyectoprogramacion/AdapterRectangulo.java
@@ -10,50 +10,50 @@ package proyectoprogramacion;
  * @author Saaku
  */
 public class AdapterRectangulo {
-    private String rect1PuntoX;
-    private String  rect1puntoY;
-    private String  rect2PuntoX;
-    private String  rect2puntoY;
+    private String Punto1X;
+    private String punto1Y;
+    private String Punto2X;
+    private String punto2Y;
     private String ID;
 
-    public AdapterRectangulo(String rect1PuntoX, String rect1puntoY, String rect2PuntoX, String rect2puntoY, String ID) {
-        this.rect1PuntoX = rect1PuntoX;
-        this.rect1puntoY = rect1puntoY;
-        this.rect2PuntoX = rect2PuntoX;
-        this.rect2puntoY = rect2puntoY;
+    public AdapterRectangulo(String Punto1X, String punto1Y, String Punto2X, String punto2Y, String ID) {
+        this.Punto1X = Punto1X;
+        this.punto1Y = punto1Y;
+        this.Punto2X = Punto2X;
+        this.punto2Y = punto2Y;
         this.ID = ID;
     }
 
-    public String getRect1PuntoX() {
-        return rect1PuntoX;
+    public String getPunto1X() {
+        return Punto1X;
     }
 
-    public void setRect1PuntoX(String rect1PuntoX) {
-        this.rect1PuntoX = rect1PuntoX;
+    public void setPunto1X(String Punto1X) {
+        this.Punto1X = Punto1X;
     }
 
-    public String getRect1puntoY() {
-        return rect1puntoY;
+    public String getPunto1Y() {
+        return punto1Y;
     }
 
-    public void setRect1puntoY(String rect1puntoY) {
-        this.rect1puntoY = rect1puntoY;
+    public void setPunto1Y(String punto1Y) {
+        this.punto1Y = punto1Y;
     }
 
-    public String getRect2PuntoX() {
-        return rect2PuntoX;
+    public String getPunto2X() {
+        return Punto2X;
     }
 
-    public void setRect2PuntoX(String rect2PuntoX) {
-        this.rect2PuntoX = rect2PuntoX;
+    public void setPunto2X(String Punto2X) {
+        this.Punto2X = Punto2X;
     }
 
-    public String getRect2puntoY() {
-        return rect2puntoY;
+    public String getPunto2Y() {
+        return punto2Y;
     }
 
-    public void setRect2puntoY(String rect2puntoY) {
-        this.rect2puntoY = rect2puntoY;
+    public void setPunto2Y(String punto2Y) {
+        this.punto2Y = punto2Y;
     }
 
     public String getID() {
@@ -64,5 +64,5 @@ public class AdapterRectangulo {
         this.ID = ID;
     }
 
-
+    
 }
